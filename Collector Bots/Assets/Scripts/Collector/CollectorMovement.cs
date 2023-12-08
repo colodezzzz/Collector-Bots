9ad68f5810656c042ce76b0f7ba997f67bb8a2ae
colodezzzz <70505345+colodezzzz@users.noreply.github.com>
2023-12-08 15:50:38 +0300

    Delete Update and Debug

--- Collector Bots/Assets/Scripts/Collector/CollectorMovement.cs
+++ Collector Bots/Assets/Scripts/Collector/CollectorMovement.cs
@@ -4,23 +4,9 @@ using UnityEngine;
 public class CollectorMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField, TextArea(8, 10)] private string _debugString;
 
     private Transform _target;
     private Vector3 _targetPosition;
-    private int _startMovingAmount = 0;
-    private int _stopMovingAmount = 0;
-
-    private void Update()
-    {
-        if (_target != null)
-        {
-            Vector3 targetPosition = new Vector3(_target.position.x, transform.position.y, _target.position.z);
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, _speed * Time.deltaTime);
-        }
-
-        _debugString = $"Collector {this}\nSpeed: {_speed}\nStarts {_startMovingAmount}\nStops {_stopMovingAmount}";
-    }
 
     private void OnDisable()
     {
@@ -29,13 +15,11 @@ public class CollectorMovement : MonoBehaviour
 
     public void StartMoving()
     {
-        _startMovingAmount++;
         StartCoroutine(Move());
     }
 
     private void StopMoving()
     {
-        _stopMovingAmount++;
         StopCoroutine(Move());
     }
 
